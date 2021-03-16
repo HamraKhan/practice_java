ee10f754ce80834ba2aeaf8181c426f2ef7f716a
Hamra Khan <hamra111@gmail.com>
2021-03-16 17:28:02 +0000

    array variables: pass by reference practice and mutability and immutability

--- practice/src/practice.java
+++ practice/src/practice.java
@@ -32,5 +32,48 @@ class practice {
         maltese.color="white";
         System.out.println(maltese.getInfo());
 
+        ArrayVariables arrayVariables = new ArrayVariables();
+
+        arrayVariables.list();
+
+        //pass by reference practice
+        final String[] toDoList = new String[4];
+        toDoList[0] = "study";
+        toDoList[1] = "finish studying";
+        toDoList[2] = "lunch";
+        toDoList[3] = "sleep";
+        printWhatsInTheArray(toDoList);
+        printAndChangeWhatsInTheArray(toDoList);
+
+
+        String[] shoppingBasket = new String[2];
+        shoppingBasket[0] = "apple";
+        shoppingBasket[1] = "chicken";
+
+        printWhatsInTheArray(shoppingBasket);
+
+        //mutability and immutability
+        String mutableText = "abcde";
+        mutableText = "12345";
+        System.out.println(mutableText);
+
+        final String mutableText2 = "abcde";
+//        mutableText2 = "12345";
+        System.out.println(mutableText2);
     }
+
+
+
+
+    private static void printWhatsInTheArray(String[] strArray) {
+        for(int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+    }
+
+    private static void printAndChangeWhatsInTheArray(String[] strArrayV2) {
+        strArrayV2[0] = "Watch TV Series";
+        printWhatsInTheArray(strArrayV2);
+    }
+
 }
